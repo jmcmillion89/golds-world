04e7592cd7adc740be624d0883888cc160cd9206
Justin McMillion <79409949+jmcmillion89@users.noreply.github.com>
2024-03-14 22:31:42 -0400

    add assets

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 const content = document.querySelector('#content')
 
+
 export default function server() {
-    content.innerText = 'Server Information'
+  content.innerText = 'Server Information'
 }
+
+// export default async function server() {
+//         // const response = await fetch("https://23.26.135.41:30120/players.json");
+//         // const info = await response.json();
+//         // console.log(info);
+//       }
+
+// 23.26.135.41:30120
+
