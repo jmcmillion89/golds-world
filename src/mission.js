3b22db7447208d80c91ac9d9d09c6b086bad46fe
Justin McMillion <79409949+jmcmillion89@users.noreply.github.com>
2024-03-16 12:09:38 -0400

    add files to edit information on pages

--- src/mission.js
+++ src/mission.js
@@ -1,18 +1,6 @@
 const content = document.querySelector('#content')
 
-export default function mission() {
-    content.innerHTML = 
-    `
-    <h1> Our mission to our community... </h1>
-
-    <p>Golds World Roleplay is, and will always be a community first and game server second. We prioritize the integrity of the roleplay experience.</p>
-    <br>
-    <p>Golds World was created by role-players, for role-players. After years of witnessing administrative abuse, quality scenes ruined with interruption. We felt it was time people had the freedom of role-play. Our intent was to create an environment for all sorts of players to gather and have fun together. </p>
-    <br>
-    <p>We strive for a fair and equal playing field across the board. Your characters and theirs choices matter the most. Yes, we do have rules in place. That is only a foundation for all of you to build your stories off of. The Rules are in place to allow an equal playing experience for all. </p>
-    <br>
-    <p>Staff is kept to a minimum and interactions with staff on an official capacity will be limited. After all, we enjoy role-play too!</p>
-    <br>   
-    <p>Our community is second to none, you all have a voice, and we are here to listen. Lastly, Welcome to the GOLDS WORLD Family. We hope you enjoy your role-play experience.</p>
-    `
+export default async function mission() {
+    const missionInfo = await (await fetch("../src/mission.html")).text();
+    content.innerHTML = missionInfo;
 }
